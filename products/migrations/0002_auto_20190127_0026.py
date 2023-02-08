4fec6617f6608dc1affe350f1aefb40edc3325af
Rouman136 <72210574+Rouman136@users.noreply.github.com>
2023-02-08 20:38:59 +0200

    Add files via upload

---
+++ products/migrations/0002_auto_20190127_0026.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='product',
+            name='photo',
+            field=models.ImageField(upload_to='products/'),
+        ),
+    ]
